9099a9f40897778bb6d201ccb6d1802f0e8ab347
smallproject <j.a.villas@live.com.ph>
2025-01-06 04:59:29 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace library_system_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

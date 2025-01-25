48e2aee6f3ba9885aa1c75f8b3ddefd35e3b9cf8
smallproject <j.a.villas@live.com.ph>
2025-01-25 03:31:37 +0100

    added book model

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,5 +2,36 @@
 {
     public class Book
     {
+        private int id;
+
+        private string name; 
+        
+        private string description;
+
+        private string isbn;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Description
+        {
+            get => description;
+            set => description = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Isbn
+        {
+            get => isbn;
+            set => isbn = value ?? throw new ArgumentNullException(nameof(value));
+        }
     }
 }

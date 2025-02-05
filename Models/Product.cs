3be097229e4ceee8e7d1aa62a44253cd65c369c5
smallproject <j.a.villas@live.com.ph>
2025-02-05 23:19:53 +0100

    temporarily push all changes

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,5 +7,35 @@
         public string description;
         public int quantity;
         public double price;
+
+        public string Id
+        {
+            get => id;
+            set => id = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Description
+        {
+            get => description;
+            set => description = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public int Quantity
+        {
+            get => quantity;
+            set => quantity = value;
+        }
+
+        public double Price
+        {
+            get => price;
+            set => price = value;
+        }
     }
 }

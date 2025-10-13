4e2c27f0bb93ef0c7754227ea92c6355bebb0690
smallproject <j.a.villas@live.com.ph>
2025-10-13 12:02:35 +0200

    register context on config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using library_system_dotnet.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();

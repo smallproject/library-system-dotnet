7ca8973c3452e051e3bd5546a2b4d564ad20fed9
smallproject <j.a.villas@live.com.ph>
2025-10-13 13:00:43 +0200

    added read dto

---
+++ Models/Dto/Book/BookReadDto.cs
@@ -0,0 +1,20 @@
+﻿namespace library_system_dotnet.Models.Dto.Book
+{
+    public class BookReadDto
+    {
+        public int Id { get; set; }
+        public string? ISBN { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public int? PublishedYear { get; set; }
+        public string? Genre { get; set; }
+        public int? CopiesAvailable { get; set; }
+        public int? TotalCopies { get; set; }
+        public string? Location { get; set; }
+        public string? CoverImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}

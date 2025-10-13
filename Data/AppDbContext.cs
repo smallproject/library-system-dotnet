590b01a636a031e629ac5c77306437de0d533185
smallproject <j.a.villas@live.com.ph>
2025-10-13 12:11:52 +0200

    setup DbContext for this project

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace library_system_dotnet.Data
+﻿using library_system_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace library_system_dotnet.Data
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional configuration can go here
+            modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired();
+        }
     }
 }

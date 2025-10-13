d1184bdabfebe2bac25143af972bb0cb42fcd4bb
smallproject <j.a.villas@live.com.ph>
2025-10-13 12:10:56 +0200

    set fields to be nullable

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,14 +2,14 @@
 
 public class Book: Base
 {
-    public string ISBN { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Author { get; set; }
-    public int PublishedYear { get; set; }
-    public string Genre { get; set; }
-    public int CopiesAvailable { get; set; }
-    public int TotalCopies { get; set; }
-    public string Location { get; set; }
-    public string CoverImageUrl { get; set; }
+    public string? ISBN { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Author { get; set; }
+    public int? PublishedYear { get; set; }
+    public string? Genre { get; set; }
+    public int? CopiesAvailable { get; set; }
+    public int? TotalCopies { get; set; }
+    public string? Location { get; set; }
+    public string? CoverImageUrl { get; set; }
 }

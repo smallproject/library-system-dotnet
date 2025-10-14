9de41ed11927420dd71ee70cc7e89bfd80564c98
smallproject <j.a.villas@live.com.ph>
2025-10-14 12:52:40 +0200

    added bookmapper

---
+++ Mappers/Book/BookMapper.cs
@@ -0,0 +1,25 @@
+﻿using library_system_dotnet.Models.Dto.Book;
+
+namespace library_system_dotnet.Mappers.Book
+{
+    public class BookMapper
+    {
+        public static BookReadDto ToReadDto(Models.Book book)
+        {
+            return new BookReadDto
+            {
+                Id = book.Id,
+                ISBN = book.ISBN,
+                Title = book.Title,
+                Description = book.Description,
+                Author = book.Author,
+                PublishedYear = book.PublishedYear,
+                Genre = book.Genre,
+                CopiesAvailable = book.CopiesAvailable,
+                TotalCopies = book.TotalCopies,
+                Location = book.Location,
+                CoverImageUrl = book.CoverImageUrl
+            };
+        }
+    }
+}

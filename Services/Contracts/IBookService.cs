9fa37c7ba1c31dc16cb78e1e9433693968b24f86
smallproject <j.a.villas@live.com.ph>
2025-10-14 10:33:56 +0200

    added GetAllAsync functionality

---
+++ Services/Contracts/IBookService.cs
@@ -0,0 +1,9 @@
+﻿using library_system_dotnet.Models.Dto.Book;
+
+namespace library_system_dotnet.Services.Contracts
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<BookReadDto>> GetAllAsync(CancellationToken ct = default)
+    }
+}

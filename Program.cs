ec58bca8e7e914a9e89c265bd1609e84d121e96d
smallproject <j.a.villas@live.com.ph>
2025-10-14 13:17:49 +0200

    register the inface and implementation of automapper

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 

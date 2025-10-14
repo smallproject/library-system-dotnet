060ca21b1b550eb57efad280134de47845c2e084
smallproject <j.a.villas@live.com.ph>
2025-10-14 13:17:11 +0200

    Implementing DI on automapper package

--- Services/Implementations/BookService.cs
+++ Services/Implementations/BookService.cs
@@ -1,6 +1,8 @@
-﻿using library_system_dotnet.Data;
+﻿using AutoMapper;
+using library_system_dotnet.Data;
 using library_system_dotnet.Models.Dto.Book;
 using library_system_dotnet.Services.Contracts;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace library_system_dotnet.Services.Implementations
@@ -8,15 +10,17 @@ namespace library_system_dotnet.Services.Implementations
     public class BookService: IBookService
     {
         private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
 
-        public BookService(AppDbContext context)
+        public BookService(AppDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
         public async Task<IEnumerable<BookReadDto>> GetAllAsync(CancellationToken ct = default)
         {
             var books = await _context.Books.ToListAsync(ct);
-            return await 
+            return _mapper.Map<IEnumerable<BookReadDto>>(books);
         }
     }
 }

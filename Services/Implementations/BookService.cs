c4c5afc32bdb03618095522d7f27f9e642571bdf
smallproject <j.a.villas@live.com.ph>
2025-10-14 12:53:01 +0200

    added book service functionality

---
+++ Services/Implementations/BookService.cs
@@ -0,0 +1,22 @@
+﻿using library_system_dotnet.Data;
+using library_system_dotnet.Models.Dto.Book;
+using library_system_dotnet.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace library_system_dotnet.Services.Implementations
+{
+    public class BookService: IBookService
+    {
+        private readonly AppDbContext _context;
+
+        public BookService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<BookReadDto>> GetAllAsync(CancellationToken ct = default)
+        {
+            var books = await _context.Books.ToListAsync(ct);
+            return await 
+        }
+    }
+}
